bf99d58484f75f474b85bce93265dd1a6db4d8ed
dipak122 <dipak717gautam@gmail.com>
2021-05-08 10:00:47 -0400

    some changes in code_submission

--- code_submission.php
+++ code_submission.php
@@ -2,6 +2,66 @@
 
 require_once('config.php');
 
+
+$fileinput = "multiple_input_case/input.txt";
+$fileoutput = "multiple_output_case/output.txt";
+echo $fileinput . "<h1>Trying</h1>";
+$code = $_POST["code"];
+echo $code;
+
+// echo fopen("$fileinput", 'r');
+// echo fopen("$fileoutput", 'r');
+if (($fh1 = fopen("$fileinput", 'r')) && ($fh2 = fopen("$fileoutput", 'r'))) {
+    // $in = "";
+    while (!feof($fh1) && !feof($fh2)) {
+        $in = fgets($fh1);
+        // echo $in;
+        $out = fgets($fh2);
+        // echo $out;
+
+        $testcase_input = "test_input_cases/" . $in;
+        echo "<br>";
+        $testcase_output = "test_cases/" . $out;
+        // $inputcontent = "";
+        // echo "<h1>after file path ...</h1>";
+
+        trim($testcase_input);
+        trim($testcase_output);
+        echo $testcase_input;
+        echo $testcase_output;
+
+        // echo fopen("$testcase_input", 'r');
+        echo "<h1>before file path ...</h1>";
+        $testcase_input = "test_input_cases/11.txt";
+        if ($inputfile = fopen("$testcase_input", 'r')) {
+            while (!feof($inputfile)) {
+                $inputcontent = fgets($inputfile);
+                echo $inputcontent;
+            }
+        } else {
+            echo "<h2>Not enter into file</h2>";
+        }
+        fclose($inputfile);
+        echo "<h1>content input ...........</h1>";
+        // echo $inputcontent . "<br>";
+
+        // echo "<h1>after file path ...</h1>";
+
+        // echo $inputcontent;
+        // echo "<br>";
+    }
+    fclose($fh1);
+
+    fclose($fh2);
+    // echo "<h1>after second File input ...........</h1>";
+
+    // echo $in . "<br>";
+    // echo $out;
+} else {
+    echo "<h1>not done</h1>";
+}
+echo "<h1>last Line</h1>";
+
 $username = $_POST['user_name'];
 $problemTitle = $_POST['problem_title'];
 
@@ -22,7 +82,7 @@ if ($result) {
     // echo $_statuss;
     // echo "<script>alert('code submitted');</script>";
     // header("Location:challenges.php?user=$usernamee/$problemTitle\");
-    header("Location:archive.php");
+    //  header("Location:archive.php");
 } else {
-    echo "Submission data not inserted <br>";
+    //  echo "Submission data not inserted <br>";
 }
