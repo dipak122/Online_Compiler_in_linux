bb234c011f035d5d887b8409f039bb893bd32077
dipak122 <dipak717gautam@gmail.com>
2021-05-24 10:38:53 -0400

    final commit

--- compilers/java.php
+++ compilers/java.php
@@ -8,7 +8,7 @@
 	$filename_error="error.txt";
 	$runtime_file="runtime.txt";
 	$executable="*.class";
-	$command=$CC." ".$filename_code;	
+	$command=$CC." ".$filename_code; ///	
 	$command_error=$command." 2>".$filename_error;
 	$runtime_error_command=$out." 2>".$runtime_file;
 
@@ -44,7 +44,7 @@
 			$output=shell_exec($out);
 		}
 	
-		echo "$output";
+		echo "$output"; 
 		//echo "<pre>$runtime_error</pre>";
 		//echo "<pre>$output</pre>";	
 		//  echo "<textarea id='div' class=\"form-control\" name=\"output\" rows=\"10\" cols=\"50\">$output</textarea><br><br>";
--- sample1.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$m = $_POST['inputdata'];
-echo "Inputs data is ";
-echo $m;
